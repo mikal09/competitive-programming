a9f6518fd0394fb302fc180f8e7d00669a0a0969
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2019-07-04 18:28:04 +0530

    binary indexed tree

---
+++ BIT.cpp
@@ -0,0 +1,20 @@
+int bit[N];
+void update(int idx, int val)
+{
+     while(idx<=n)
+     {
+          bit[idx]+=val;
+          idx+=idx&-idx;
+     }
+}
+int pref(int idx)
+{
+     int ans=0;
+     while(idx>0)
+     {
+          ans+=bit[idx];
+          idx-=idx&-idx;
+     }
+     return ans;
+}
+int rsum(int l, int r){ return pref(r) - pref(l-1); }
