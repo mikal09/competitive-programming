92c642d2d427e115b3bd8a80b4c62ba25a68bacd
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2019-08-30 13:57:42 +0530

    lazy segment tree

--- segmen_tree_lazy.cpp
+++ segmen_tree_lazy.cpp
@@ -1,46 +1,42 @@
-struct data
+struct info
 {
      int x;
-     data () : x(0) {};
+     info () : x(0) {};
 };
 struct SegTree
 {
      int N;
-     vector<data> st;
-     vector<bool> cLazy;
+     vector<info> st;
      vector<int> lazy;
 
      void init(int n)
      {
           N = n;
           st.resize(4 * N + 5);
-          cLazy.assign(4 * N + 5, false);
           lazy.assign(4 * N + 5, 0);
      }
 
-     void merge(data &cur, data &l, data &r) 
+     void merge(info &cur, info &l, info &r) 
      {
           cur.x = l.x + r.x;
      }
      
      void propagate(int node, int L, int R)
      {
-          if(L != R)
-          {
-               cLazy[node*2] = 1;
-               cLazy[node*2 + 1] = 1;
-               lazy[node*2] = lazy[node];
-               lazy[node*2 + 1] = lazy[node]; 
-          }
           st[node].x += lazy[node]*(R-L+1);
-          cLazy[node] = 0;
+          if(L!=R) 
+          {
+               lazy[node*2] += lazy[node]; 
+               lazy[node*2+1] += lazy[node];
+          }
+          lazy[node] = 0;
      }
 
      void Build(int node, int L, int R)
      {
           if(L==R)
           {
-               st[node].x = a[L];
+               st[node].x = 0;
                return;
           }
           int M=(L + R)/2;
@@ -49,32 +45,33 @@ struct SegTree
           merge(st[node], st[node*2], st[node*2+1]);
      }
 
-     data Query(int node, int L, int R, int i, int j)
+     info Query(int node, int L, int R, int i, int j)
      {
-          if(cLazy[node])
+          if(lazy[node])
                propagate(node, L, R);
           if(j<L || i>R)
-               return data();
+               return info();
           if(i<=L && R<=j)
                return st[node];
+
           int M = (L + R)/2;
-          data left=Query(node*2, L, M, i, j);
-          data right=Query(node*2 + 1, M + 1, R, i, j);
-          data cur;
+          info left=Query(node*2, L, M, i, j);
+          info right=Query(node*2 + 1, M + 1, R, i, j);
+          info cur;
           merge(cur, left, right);
           return cur;
      }   
 
      void Update(int node, int L, int R, int i, int j, int val)
      {
-          if(cLazy[node])
+          if(lazy[node])
                propagate(node, L, R);
           if(j<L || i>R)
                return;
+
           if(i<=L && R<=j)
           {
-               cLazy[node] = 1;
-               lazy[node] = val;
+               lazy[node]+= val;
                propagate(node, L, R);
                return;
           }
@@ -84,7 +81,7 @@ struct SegTree
           merge(st[node], st[node*2], st[node*2 + 1]);
      }
 
-     data query(int l, int r) { return Query(1, 1, N, l, r); }
+     info query(int l, int r) { return Query(1, 1, N, l, r); }
 
      void update(int l, int r, int val) { Update(1, 1, N, l, r, val); }
 
