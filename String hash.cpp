9c62bac6785c6342f37aa615f9240b0906de72d2
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2019-11-06 16:45:20 +0530

    string to hash

--- String hash.cpp
+++ String hash.cpp
@@ -11,3 +11,26 @@ long long compute_hash(string const& s)
     }
     return hash_value;
 }
+
+
+
+
+set <int> ans;
+void compute_hash(string s)
+{
+     const int p = 31, p1 = 53;
+     const int m = 1e9 + 9;
+     int hash_value = 0, hash1_value = 0;
+     int p_pow = 1, p1_pow = 1;
+
+     for (char c : s)
+     {
+          hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+          p_pow = (p_pow * p) % m;
+          hash1_value = (h1ash_value + (c - 'a' + 1) * p1_pow) % m;
+          p1_pow = (p1_pow * p1) % m;
+
+          ans.insert({hash_value, hash1_value});
+
+     }
+}
