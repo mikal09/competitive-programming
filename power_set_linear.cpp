fbe938ec82cd1b2d4e984baa18725cb1b5a7174f
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2020-01-24 20:42:31 +0530

    Create power_set_linear.cpp

---
+++ power_set_linear.cpp
@@ -0,0 +1,14 @@
+void subsets(int x)
+{
+     int range = 1LL << x;
+
+     for (int mask = 0; mask < range; mask++)
+     {
+          for (int i = 0; i < x; i++)
+          {
+               if (mask & (1LL << i))
+                    
+          }
+     }
+
+}
