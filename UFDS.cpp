bd26d1795f0e1d1b43802a64f66b302bf2f6dd7f
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2020-01-25 00:46:36 +0530

    Update UFDS.cpp

--- UFDS.cpp
+++ UFDS.cpp
@@ -1,39 +1,38 @@
 struct DSU
 {
-     vector <int> uf, sz;
+	vector <int> uf, sz;
+	int cnt, weight;
 
-     void init(int n)
-     {
-          uf.resize(n + 1);
-          sz.resize(n + 1);
-          for (int i = 0; i <= n; i++)
-               uf[i] = i, sz[i] = 1;
-     }
+	void init(int n)
+	{
+		uf.resize(n + 1);
+		sz.resize(n + 1);
+		cnt = n; weight = 0;
+		for (int i = 0; i <= n; i++)
+			uf[i] = i, sz[i] = 1;
+	}
+	int root(int i)
+	{
+		while (uf[i] != i)
+		{
+			uf[i] = uf[uf[i]];
+			i = uf[i];
+		}
+		return i;
+	}
+	void Union(int A, int B, int w)
+	{
+		int root_A = root(A);
+		int root_B = root(B);
 
-     int root(int i)
-     {
-          while (uf[i] != i)
-          {
-               uf[i] = uf[uf[i]];
-               i = uf[i];
-          }
-          return i;
-     }
+		if (root_B == root_A)
+			return ;
 
-     void Union(int A, int B)
-     {
-          int root_A = root(A);
-          int root_B = root(B);
+		if (sz[root_A] < sz[root_B ])
+			swap(root_A, root_B);
 
-          if (root_B == root_A)
-               return ;
-
-          if (sz[root_A] < sz[root_B ])
-               swap(root_A, root_B);
-
-          uf[root_B] = uf[root_A];
-          sz[root_A] += sz[root_B];
-
-     }
-
+		cnt--; weight += w;
+		uf[root_B] = uf[root_A];
+		sz[root_A] += sz[root_B];
+	}
 };
