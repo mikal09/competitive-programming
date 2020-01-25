6e5a671e2f3d66c7db4a5558f04a53caef9a1d08
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2020-01-26 03:34:07 +0530

    Create DFS matrix.cpp

---
+++ DFS matrix.cpp
@@ -0,0 +1,23 @@
+int vis[N][N];
+int dx[4] = { -1, +1, 0, 0};
+int dy[4] = {0, 0, +1, -1};
+
+void dfs(int x, int y)
+{
+	if (x == n && y == m)
+		return ;
+
+	vis[x][y] = 1;
+	for (int dir = 0; dir < 4; dir++)
+	{
+		int x1 = x + dx[dir];
+		int y1 = y + dy[dir];
+		if (x1 < 1 || y1 < 1 || x1 > n || y1 > m) 
+			continue;
+
+		if (!vis[x1][y1])
+		{
+			dfs(x1, y1);
+		}
+	}
+}
