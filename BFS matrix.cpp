75ef943a23428f8094379cedd31fbe34662cea3d
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2020-01-26 03:33:09 +0530

    Update BFS matrix.cpp

--- BFS matrix.cpp
+++ BFS matrix.cpp
@@ -1,27 +1,28 @@
 int vis[N][N];
-int dx[4]={-1, +1, 0, 0};
-int dy[4]={0, 0, +1, -1};
+int dx[4] = { -1, +1, 0, 0};
+int dy[4] = {0, 0, +1, -1};
 
 void bfs(int x, int y, int n, int m)
 {
      queue<pair<int, int> > q;
      q.push({x, y});
-     vis[x][y]=1;
-     
-     while(!q.empty())
-     {
-          int x=q.front().first;
-          int y=q.front().second;
-          q.pop();
-          for(int dir=0;dir<4;dir++)
-          {
-               int x1=x+dx[dir];
-               int y1=y+dy[dir];
-               if(x1<1 || y1<1 || x1>n || y1>m) continue;
+     vis[x][y] = 1;
 
-               if(!vis[x1][y1])
+     while (!q.empty())
+     {
+          int x = q.front().first;
+          int y = q.front().second;
+          q.pop();
+          for (int dir = 0; dir < 4; dir++)
+          {
+               int x1 = x + dx[dir];
+               int y1 = y + dy[dir];
+               if (x1 < 1 || y1 < 1 || x1 > n || y1 > m) 
+                    continue;
+
+               if (!vis[x1][y1])
                {
-                    vis[x1][y1]=1;
+                    vis[x1][y1] = 1;
                     q.push({x1, y1});
                }
           }
