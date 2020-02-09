ad7f5198e25aefa98a0a8353cd0040ae63224ac3
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2020-02-09 19:28:14 +0530

    Update NCR.cpp

--- NCR.cpp
+++ NCR.cpp
@@ -1,29 +1,34 @@
-int fact[N], mod = 1e9 + 7;
-inline int add(int x, int y) { x += y; if (x >= mod) x -= mod; return x; }
-inline int sub(int x, int y) { x -= y; if (x < 0) x += mod; return x; }
-inline int mul(int x, int y) { return ((x % mod) * (y % mod)) % mod; }
+const int mod = 1e9 + 7;
+int fact[N], invfact[N];
+inline int add(int x, int y) { x += y; return x % mod;}
+inline int sub(int x, int y) { x -= y; x %= mod; x += mod; return x % mod;}
+inline int mul(int x, int y) { return (x * y) % mod;}
 inline int powr(int a, int b) {
-     int x = 1 % mod;
-     while (b) {
-          if (b & 1) x = mul(x, a);
-          a = mul(a, a);
-          b >>= 1;
-     }
-     return x;
+	int x = 1 % mod;
+	b %= (mod - 1);
+	while (b) {
+		if (b & 1) x = mul(x, a);
+		a = mul(a, a);
+		b >>= 1;
+	}
+	return x;
 }
-inline int inv(int a) { return powr(a, mod - 2); }
+inline int inv(int a) { return powr(a, mod - 2);}
+void pre()
+{
+	fact[0] = invfact[0] = 1;
+	for (int i = 1; i < N; i++)
+		fact[i] = mul(i, fact[i - 1]);
 
-void precompute()
-{
-     fact[0] = 1;
-     for (int i = 1; i < N; i++)
-          fact[i] = mul(fact[i - 1], i);
+	invfact[N - 1] = inv(fact[N - 1]);
+	for (int i = N - 2; i >= 1; i--)
+		invfact[i] = mul(invfact[i + 1], i + 1);
+
+	assert(invfact[1] == 1);
 }
-int nCr(int num, int den)
+inline int nCr(int n, int k)
 {
-     int den1 = fact[den], den2 = fact[num - den];
-     num = fact[num];
-     den = mul(den1, den2);
-     int ans = mul(num, inv(den));
-     return ans;
+	if (n < k || k < 0)
+		return 0;
+	return mul(fact[n], mul(invfact[k], invfact[n - k]));
 }
