ac3f85eeb7a3e56f32fd9c2b9302e9325c4f3041
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2020-02-12 18:47:46 +0530

    Update digit dp structure.cpp

--- digit dp structure.cpp
+++ digit dp structure.cpp
@@ -20,3 +20,30 @@ int dp(int pos, int free)
 
      return ans;
 }
+
+// finds solution from L to R. Make required changes according to the probelm;
+
+int dp(int pos, int bara, int chota)
+{
+     if (pos == n)
+          return 1;
+
+     int &ans = cache[pos][bara][chota];
+     if (ans != -1)
+          return ans;
+
+     ans = 0;
+     int st = (bara ? -1 : s[pos] - '0');
+     int en = (chota ? 10 : s1[pos] - '0');
+
+     for (int i = st + 1; i < en; i++)
+          ans += dp(pos + 1, 1, 1);
+
+     if (!bara)
+          ans += dp(pos + 1, 0, (chota ? 1 : (st != en)));
+
+     if (!chota and (bara or st != en))
+          ans += dp(pos + 1, (bara ? 1 : (st != en)), 0);
+
+     return ans;
+}
