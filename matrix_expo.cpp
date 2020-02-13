42b2416187b35137e9ea587a50dea705b7c340a8
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2020-02-13 18:41:53 +0530

    Update matrix_expo.cpp

--- matrix_expo.cpp
+++ matrix_expo.cpp
@@ -1,17 +1,22 @@
+
 const int M = 5;
 struct mat_expo
 {
      int m[M][M];
 
      mat_expo() { memset(m, 0, sizeof m); }
-
-     void eye() { lop(i, 0, M) m[i][i] = 1; }
-
+     void eye()
+     {
+          for (int i = 0; i < M; i++)
+               m[i][i] = 1;
+     }
      mat_expo operator*(const mat_expo &a) const
      {
           mat_expo r;
-          lop(i, 0, M) lop(j, 0, M) lop(k, 0, M)
-          r.m[i][j] = (r.m[i][j] + m[i][k] * 1ll * a.m[k][j]) % mod;
+          for (int i = 0; i < M; i++)
+               for (int j = 0; j < M; j++)
+                    for (int k = 0; k < M; k++)
+                         r.m[i][j] = (r.m[i][j] + m[i][k] * 1ll * a.m[k][j]) % mod;
           return r;
      }
 };
