32d415a24854c2e9e8e9026cb5d015f6b1e61eb4
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2020-02-20 02:36:07 +0530

    Create Topological Sort and SCC.cpp

---
+++ Topological Sort and SCC.cpp
@@ -0,0 +1,43 @@
+vi g[N], gr[N], order, tps, sc;
+int vis[N];
+
+void dfs(int i)
+{
+     vis[i] = 1;
+     for (auto x : g[i])
+          if (!vis[x])
+               dfs(x);
+     order.pb(i);
+}
+void dfs1(int i)
+{
+     vis[i] = 1;
+     sc.pb(i);
+     for (auto x : gr[i])
+          if (!vis[x])
+               dfs1(x);
+}
+bool cycle()
+{
+     for (int i = 1; i <= n + m; i++)
+          if (!vis[d.root(i)])
+               dfs((d.root(i)));
+
+     // if there is no loop in the graph then tsp store topological sort of the graph
+     tps = order;
+     reverse(all(tps));
+     memset(vis, 0, sizeof vis);
+     int mx = 0;
+
+     while (sz(order)) // this part is for strongly connected component
+     {
+          sc.clear();
+          dfs1(order.back());
+          while (sz(order) && vis[order.back()])
+               order.pop_back();
+
+          mx = max(mx, sz(sc));
+     }
+
+     return (mx > 1);
+}
