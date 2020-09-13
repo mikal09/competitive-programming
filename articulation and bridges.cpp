61a4b51c424af68772d92cb061289b577166ad71
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2020-09-13 13:57:23 +0530

    Create articulation and bridges.cpp

---
+++ articulation and bridges.cpp
@@ -0,0 +1,40 @@
+vector<int> g[N],art_point;
+int n, t[N], t1[N], tim;
+vector <pair<int,int>> edges;
+
+// bridges
+void bridges(int i, int p)
+{
+     t[i] = t1[i] = ++tim;
+
+     for (auto x : g[i])
+     {
+          if (!t[x])
+               bridges(x, i);
+          if (x != p)
+               t1[i] = min(t1[i], t1[x]);
+     }
+     for (auto x : g[i])
+          if (t1[i] > t[x])
+               edges.pb(i,x);
+}
+
+// articulation point
+void bridges(int i, int p)
+{
+     t[i] = t1[i] = ++tim;
+
+     for (auto x : g[i])
+     {
+          if (!t[x])
+               bridges(x, i);
+          if (x != p)
+               t1[i] = min(t1[i], t1[x]);
+     }
+     for (auto x : g[i])
+          if (t1[i] > t[x])
+          {
+               art_point.pb(x);
+               break;
+          }
+}
