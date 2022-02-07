ee9a73150faf836af37e7f0a68a313aeb509b0a9
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2022-02-08 01:48:51 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,27 +1,31 @@
-vpi v[N];
-vi dis;
+const int INF = 1000000000;
+vector<vector<pair<int, int>>> adj;
 
-void Dijkstra(int r, int n) 
-{
-     priority_queue<pii, vector<pii>, greater<pii> > Q;  
-     dis.assign(n+1,1e18);
-     dis[r] = 0;
-     Q.push({0,r});
+void dijkstra(int s, vector<int> & d, vector<int> & p) {
+    int n = adj.size();
+    d.assign(n, INF);
+    p.assign(n, -1);
 
-     while(!Q.empty())
-     {
-        int u = Q.top().second;
-        Q.pop();
-        for(auto &c : v[u])
-        {
-            int x = c.first;
-            int w = c.second;
-            if(dis[x] > dis[u]+w)
-            {
-                dis[x] = dis[u]+w;
-                Q.push({dis[x],x});
+    d[s] = 0;
+    using pii = pair<int, int>;
+    priority_queue<pii, vector<pii>, greater<pii>> q;
+    q.push({0, s});
+    while (!q.empty()) {
+        int v = q.top().second;
+        int d_v = q.top().first;
+        q.pop();
+        if (d_v != d[v])
+            continue;
+
+        for (auto edge : adj[v]) {
+            int to = edge.first;
+            int len = edge.second;
+
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+                q.push({d[to], to});
             }
         }
-     }
-
+    }
 }
