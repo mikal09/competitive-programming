ef0a7d4d9540b1431e684fd7b7a754e417675475
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2022-03-12 00:43:25 +0530

    Update mod_inv.cpp

--- mod_inv.cpp
+++ mod_inv.cpp
@@ -15,3 +15,33 @@ int modInverse(int a, int m)
 
     return x; 
 } 
+
+int gcdExtended(int a, int b, int* x, int* y)
+{
+     if (a == 0)
+     {
+          *x = 0, *y = 1;
+          return b;
+     }
+     
+     int x1, y1;
+     int gcd = gcdExtended(b % a, a, &x1, &y1);
+
+     *x = y1 - (b / a) * x1;
+     *y = x1;
+
+     return gcd;
+}
+
+void modInverse(int a, int m)
+{
+     int x, y;
+     int g = gcdExtended(a, m, &x, &y);
+     if (g != 1)
+          cout << "Inverse doesn't exist";
+     else
+     {
+          int res = (x % m + m) % m;
+          cout << "Modular multiplicative inverse is " << res;
+     }
+}
