7f6a512000e435871e6012b487037aaaa5cbc5db
Saurabh Kumar <31901790+mikal09@users.noreply.github.com>
2022-03-12 15:33:15 +0530

    Update euler's totient function.cpp

--- euler's totient function.cpp
+++ euler's totient function.cpp
@@ -13,3 +13,6 @@ int euler_totient(int num)
           total -= total / num;
      return total;
 }
+
+// The formula basically says that the value of Φ(n) is equal to n multiplied by-product of (1 – 1/p) for all prime factors p of n. 
+//      For example value of Φ(6) = 6 * (1-1/2) * (1 – 1/3) = 2.
